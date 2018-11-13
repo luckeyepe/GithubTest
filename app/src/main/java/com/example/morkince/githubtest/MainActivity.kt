a10f0590e4df8eac93e30d590759895cba7f2b4b
luckeyepe <Gorilaman2>
2018-11-13 15:35:18 +0800

    added message button

--- app/src/main/java/com/example/morkince/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/morkince/githubtest/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.morkince.githubtest
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_mainHello.setOnClickListener {
+            var alertDialog = AlertDialog.Builder(this)
+            alertDialog.setCancelable(false)
+            alertDialog.setMessage("Hello World")
+            alertDialog.setPositiveButton("OK") { dialog, which -> dialog.dismiss() }
+
+            alertDialog.show()
+        }
     }
 }
