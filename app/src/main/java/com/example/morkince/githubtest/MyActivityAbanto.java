13063e55732008271cb2608a66855846713d0486
Japhet Titus Abanto <ondoyjet2@gmail.com>
2018-11-14 21:32:29 +0800

    added objects in my activity titus

---
+++ app/src/main/java/com/example/morkince/githubtest/MyActivityAbanto.java
@@ -0,0 +1,18 @@
+package com.example.morkince.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MyActivityAbanto extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_abanto);
+        refs();
+    }
+    public void refs()
+    {
+
+    }
+}
--- app/src/main/java/com/example/morkince/githubtest/Titus_Activity.java
+++ app/src/main/java/com/example/morkince/githubtest/Titus_Activity.java
@@ -1,5 +1,6 @@
 package com.example.morkince.githubtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class Titus_Activity extends AppCompatActivity {
-Button alertDialog,cancelActivity;
+Button alertDialog,cancelActivity,alertTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +16,20 @@ Button alertDialog,cancelActivity;
         ref();
         alertDialog.setOnClickListener(alert);
         cancelActivity.setOnClickListener(cancelAct);
+        alertTwo.setOnClickListener(alert2);
     }
     public View.OnClickListener alert = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Toast.makeText(Titus_Activity.this, "Japhet Titus Abanto", Toast.LENGTH_LONG).show();
+        }
+    };
+    public View.OnClickListener alert2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent myIntent = new Intent(Titus_Activity.this,
+                    MyActivityAbanto.class);
+            startActivity(myIntent);
         }
     };
     public View.OnClickListener cancelAct = new View.OnClickListener() {
@@ -31,6 +41,8 @@ Button alertDialog,cancelActivity;
     public void ref()
     {
         alertDialog =findViewById(R.id.btn_titusAlert);
+        alertTwo =findViewById(R.id.btn_titusAlert2);
         cancelActivity= findViewById(R.id.btn_titusCancel);
+
     }
 }
