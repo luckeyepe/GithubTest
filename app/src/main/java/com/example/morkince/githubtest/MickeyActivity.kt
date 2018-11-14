ca5db15f695f8b8fa6db7971c57343595012eb7d
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2018-11-14 21:16:59 +0800

    added functionalities to mickey activity

--- app/src/main/java/com/example/morkince/githubtest/MickeyActivity.kt
+++ app/src/main/java/com/example/morkince/githubtest/MickeyActivity.kt
@@ -1,12 +1,24 @@
 package com.example.morkince.githubtest
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_mickey.*
 
 class MickeyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mickey)
+
+        btn_mickeyAlert.setOnClickListener {
+            var alertDialog = AlertDialog.Builder(this)
+            alertDialog.setCancelable(false)
+            alertDialog.setMessage("Hello there my name is Mickey Mouse")
+            alertDialog.setPositiveButton("YEEEAH") { dialog, _ -> dialog.dismiss() }
+
+            alertDialog.show()
+        }
     }
 }
