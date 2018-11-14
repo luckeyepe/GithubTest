fa6186d107abe6201dfb78370bdd09e6a1dbb471
Japhet Titus Abanto <ondoyjet2@gmail.com>
2018-11-14 21:19:32 +0800

    added objects in my activity titus

--- app/src/main/java/com/example/morkince/githubtest/Titus_Activity.java
+++ app/src/main/java/com/example/morkince/githubtest/Titus_Activity.java
@@ -2,12 +2,35 @@ package com.example.morkince.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Titus_Activity extends AppCompatActivity {
-
+Button alertDialog,cancelActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_titus_);
+        ref();
+        alertDialog.setOnClickListener(alert);
+        cancelActivity.setOnClickListener(cancelAct);
+    }
+    public View.OnClickListener alert = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(Titus_Activity.this, "Japhet Titus Abanto", Toast.LENGTH_LONG).show();
+        }
+    };
+    public View.OnClickListener cancelAct = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        finish();
+        }
+    };
+    public void ref()
+    {
+        alertDialog =findViewById(R.id.btn_titusAlert);
+        cancelActivity= findViewById(R.id.btn_titusCancel);
     }
 }
